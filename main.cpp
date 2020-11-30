b063b1ce9dd69a00f483b79855d1bf0d1b20b6e5
Sarsaparilla29 <jacobthebennett@gmail.com>
2020-11-29 21:54:14 -0500

    added map, objects,pointers

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include "cell.h"
+#include "position.h"
+#include "pathfinder.h"
 
-using namespace std;
+int main(){
+    // cell objects
+    cell cellObj0 = 0;
+    cell cellObj1 = 1;
+    cell cellObj2 = 2;
+    cell cellObj3 = 3;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+    // pathfinder object
+    pathfinder pathObj;
+
+    // array for map
+    int arr[5][6] = {
+        {cellObj1,cellObj1,cellObj1,cellObj1,cellObj1,cellObj1}
+        {cellObj0,cellObj0,cellObj0,cellObj0,cellObj0,cellObj1}
+        {cellObj1,cellObj1,cellObj1,cellObj1,cellObj0,cellObj1}
+        {cellObj1,cellObj0,cellObj0,cellObj0,cellObj0,cellObj1}
+        {cellObj1,cellObj2,cellObj1,cellObj1,cellObj1,cellObj1}
+    };
+
+    // pointer to array
+    int *arrPtr;
+    &arrPtr = arr;
+    pathObj.search();
+
+
+    /*int arr[5][6] = {
+        {1,1,1,1,1,1}
+        {0,0,0,0,0,1}
+        {1,1,1,1,0,1}
+        {1,0,0,0,0,1}
+        {1,2,1,1,1,1}
+    };*/
+
+
+
+
+
     return 0;
 }
