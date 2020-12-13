6bcec1098087605a86a3769478289bd14538cc3a
Jacob Bennett <57199778+Sarsaparilla29@users.noreply.github.com>
2020-12-13 18:01:55 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,18 @@ int main(){
     // create map
     cell fullmap[5][6];
     // fill the map with walls (1)
-    for(int i = 0; i < 5; i++)
-        for(int j = 0; j < 6; j++)
+    for(int i = 0; i < 6; i++)
+        for(int j = 0; j < 7; j++)
             fullmap[i][j].setCellType(1);
+
+    std::cout << "After setting all cells to wall(1)\n";
+    for(int i = 0; i < 6; i++){
+        std::cout << " ";
+        for(int j = 0; j < 7; j++){
+            std::cout << fullmap[i][j].getCellType() << " ";
+        }
+        std::cout << "\n";
+    }
 
     // updating specific cells with empty(0) or goal(2) values
     fullmap[1][0].setCellType(0);
@@ -26,11 +35,23 @@ int main(){
     fullmap[3][2].setCellType(0);
     fullmap[4][2].setCellType(0);
     fullmap[5][2].setCellType(2);
+    //fullmap[0][6].setCellType(1); // set manually to a wall(1)
+
+    //fullmap[0][6].setCellType(1); // for some reason this cell changes to 0 when [1][0] is set to 0
     // initial position will be 1,0
     
-    pathfinder pf(&fullmap[0][0],1,0);
+    std::cout << "Created Path\n";
+    for(int i = 0; i < 6; i++){
+        std::cout << " ";
+        for(int j = 0; j < 7; j++){
+            std::cout << fullmap[i][j].getCellType() << " ";
+        }
+        std::cout << "\n";
+    }
+    
+    pathfinder pf(&fullmap[0][0],0,1);
     pf.search();
-    std::cout << "END";
+    std::cout << "END\n";
 
 
 
@@ -43,5 +64,5 @@ int main(){
     //pathObj.search();
 
     // end of program
-    return 0;
+    //return 0;
 }
