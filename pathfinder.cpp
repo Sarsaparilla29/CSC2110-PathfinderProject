88f49bc61fe1c8245044f53b33aed3873d3f4248
Jacob Bennett <57199778+Sarsaparilla29@users.noreply.github.com>
2020-12-13 21:37:33 -0500

    Update pathfinder.cpp

--- pathfinder.cpp
+++ pathfinder.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
-#include "pathfinder.h"
 #include <vector>
 #include <string>
+#include <sstream>
+#include "pathfinder.h"
 
 void pathfinder::display()
 {
-	cout << "(" << currentPosition.getColumn() << currentPosition.getRow() << ") -> ";
+	cout << "(" << currentPosition.getColumn() << "," << currentPosition.getRow() << ") -> ";
 }
 
 void pathfinder::search(){
     // debugging statement
-    std::cout << "reached search function" << std::endl;
+    //std::cout << "reached search function" << std::endl;
     // call the dfs algorithm
     std::vector<std::string> solution;
 
     if(dfs(currentPosition, solution)) {
         // print the solution
-        std::cout << solution.back() << "\n" << std::endl;
+        //std::cout << solution.back() << "\n" << std::endl;
         // how we can add the inital position to the vector?
 
         std::cout << "Solution: ";
-        for(int i = solution.size(); i >= 0; i--){
-            std::cout << solution[i] << std::endl;
+        // reverse vector output 
+        for (auto rvout = solution.crbegin(); rvout != solution.crend(); ++rvout) {
+            std::cout << "\n" << *rvout;
         }
     }
     else{
@@ -32,14 +34,19 @@ void pathfinder::search(){
 //            pos   x-structure  x-boundaries 
 bool pathfinder::dfs(position p, std::vector<std::string>& v){
     // debugging statement
-    std::cout << "reached dfs function" << std::endl;
+    //std::cout << "reached dfs function" << std::endl;
     const int rowSize = 5;
     const int columnSize = 6;
     
     cell* currentCell = map + (p.getRow()*columnSize + p.getColumn());
+
+    //std::string fRow = (std::string)p.getRow();
+    //std::string fColumn = (std::string)p.getColumn();
+    std::stringstream buffer;
+    buffer << "(" << p.getRow() << "," << p.getColumn() << ")" << " goal";
     // Check if this state is the goal (solution)
     if(currentCell->getCellType() == 2) { // is this the goal?
-        v.push_back("currentPosition"); // position in the format (leftPosition.getRow(), leftPosition.getColumn())->
+        v.push_back(buffer.str()); // position in the format (leftPosition.getRow(), leftPosition.getColumn())->
         return true;
     }
     // Mark this state as visited(3)
@@ -49,7 +56,7 @@ bool pathfinder::dfs(position p, std::vector<std::string>& v){
     //                   up (decrease row value),      down (increase row value)
     
     // 1) is the next position within the array boundaries
-    // 2) cells that the alogorithm should not visit
+    // 2) cells that the algorithm should not visit
     //    a) wall
     //    b) visited
     // 3) check whether next position contains the path to the solution 
@@ -69,81 +76,41 @@ bool pathfinder::dfs(position p, std::vector<std::string>& v){
 
     if(p.getColumn()-1 >=0 && leftCell->getCellType()!=1 && leftCell->getCellType()!=3 && dfs(leftPosition,v)){ // left
         // this is part of the path of the solution
-        std::cout << "leftPosition\n";
-        v.push_back("leftPosition"); // position in the format (leftPosition.getRow(), leftPosition.getColumn())->
+        //std::cout << "leftPosition\n";
+        std::stringstream leftBuffer;
+        leftBuffer << "(" << p.getRow() << "," << p.getColumn() << ")" << " left";
+        v.push_back(leftBuffer.str()); // position in the format (leftPosition.getRow(), leftPosition.getColumn())->
         return true;
     }
     else if(p.getColumn()+1 >=0 && rightCell->getCellType()!=1 && rightCell->getCellType()!=3 && dfs(rightPosition,v)) { // right
-        std::cout << "rightPosition\n";
-        v.push_back("rightPosition"); // position in the format (leftPosition.getRow(), leftPosition.getColumn())->
+        //std::cout << "rightPosition\n";
+        std::stringstream rightBuffer;
+        rightBuffer << "(" << p.getRow() << "," << p.getColumn() << ")" << " right";
+        v.push_back(rightBuffer.str()); // position in the format (leftPosition.getRow(), leftPosition.getColumn())->
         return true;
     }
     else if (p.getRow()-1 >=0 && upCell->getCellType()!=1 && upCell->getCellType()!=3 && dfs(upPosition,v)) { // up
-        std::cout << "upPosition\n";
-        v.push_back("upPosition"); // position in the format (leftPosition.getRow(), leftPosition.getColumn())->
+        //std::cout << "upPosition\n";
+        std::stringstream upBuffer;
+        upBuffer << "(" << p.getRow() << "," << p.getColumn() << ")" << " up";
+        v.push_back(upBuffer.str()); // position in the format (leftPosition.getRow(), leftPosition.getColumn())->
         return true;
     }
     else if (p.getRow()+1 >=0 && downCell->getCellType()!=1 && downCell->getCellType()!=3 && dfs(downPosition,v)) { // down
-        std::cout << "downPosition\n";
-        v.push_back("downPosition"); // position in the format (leftPosition.getRow(), leftPosition.getColumn())->
+        //std::cout << "downPosition\n";
+        std::stringstream downBuffer;
+        downBuffer << "(" << p.getRow() << "," << p.getColumn() << ")" << " down";
+        v.push_back(downBuffer.str()); // position in the format (leftPosition.getRow(), leftPosition.getColumn())->
         return true;
     }
 
     return false;
 }
 
-pathfinder::pathfinder(cell* pmap, int inC, int inR) //constructor
+pathfinder::pathfinder(cell* pmap, int inR, int inC) //constructor
 {
 	map = pmap;
 	currentPosition.setColumn(inC);
 	currentPosition.setRow(inR);
 }
 
-/*void pathfinder::search() //Function for the schmooving
-{
-	do //Still wrapping my head around this, hopefully we can discuss during the next meeting
-	{
-		if (currentPosition.getColumn() < *map)
-		{
-			if (checkCellType(currentPosition.getColumn() + 1) == 0)
-			{
-				currentPosition.setCellType(3); //Marks it as visited
-			}
-
-			else if (checkCellType(currentPosition.getColumn() + 1) == 2)
-			{
-				break; //Ends the function call to end at the goal
-			}
-
-			else if (checkCellType(currentPosition.getColumn() + 1) == 3)
-			{
-				cout << "We been here cuz."; //Something when the programs goes to an already visted cell
-			}
-		}
-		
-	} while(currentPosition.getRow() != 1 || currentPosition.getColumn() != 1); //Stops when met with a wall
-}*/
-
-
-/*int pathfinder::checkCellType(int c) //Function to check what type of cell we're at
-{
-	switch (c)
-	{
-	case 0:
-		cout << "This cell is empty";
-		return 0;
-		break;
-	case 1:
-		cout << "This is a wall";
-		return 1;
-		break;
-	case 2:
-		cout << "This is the goal";
-		return 2;
-		break;
-	case 3:
-		cout << "I've been here before";
-		return 3;
-		break;
-	}
-}*/
