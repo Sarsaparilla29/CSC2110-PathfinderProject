a9c23ca35ee41361bfea8720d4e3ce345080602c
Sarsaparilla29 <jacobthebennett@gmail.com>
2020-12-13 22:41:43 -0500

    new map :^)

--- main.cpp
+++ main.cpp
@@ -22,19 +22,38 @@ int main(){
     }
 
     // updating specific cells with empty(0) or goal(2) values
-    fullmap[1][0].setCellType(0);
+    // row 0
+    //fullmap[0][3].setCellType(0);
+    //fullmap[1][0].setCellType(0);
+    // row 1
     fullmap[1][1].setCellType(0);
     fullmap[1][2].setCellType(0);
-    fullmap[1][3].setCellType(0);
+    //fullmap[1][3].setCellType(0);
     fullmap[1][4].setCellType(0);
     fullmap[1][5].setCellType(0);
-    fullmap[2][5].setCellType(0);
-    fullmap[3][5].setCellType(0);
+    // row 2
+    fullmap[2][0].setCellType(0);
+    //fullmap[2][1].setCellType(0);
+    fullmap[2][2].setCellType(0);
+    fullmap[2][3].setCellType(0);
+    fullmap[2][4].setCellType(0);
+    //fullmap[2][5].setCellType(0);
+    // row 3
+    fullmap[3][0].setCellType(0);
+    fullmap[3][1].setCellType(0);
+    fullmap[3][2].setCellType(0);
+    //fullmap[3][3].setCellType(0);
     fullmap[3][4].setCellType(0);
+    //fullmap[3][5].setCellType(0);
+    // row 4
+    fullmap[4][4].setCellType(0);
+    fullmap[4][5].setCellType(2);
+    
+    /*fullmap[3][4].setCellType(0);
     fullmap[3][3].setCellType(0);
     fullmap[3][2].setCellType(0);
     fullmap[4][2].setCellType(0);
-    fullmap[4][1].setCellType(2);
+    fullmap[4][1].setCellType(2);*/
     // need to rebuild solution in VS every time a change is made to the map
 
     // initial position will be (1,0)
@@ -48,8 +67,9 @@ int main(){
         std::cout << "\n";
     }
     
-    pathfinder pf(&fullmap[0][0],1,0);
+    pathfinder pf(&fullmap[0][0],1,5);
     pf.search();
+    // after searching for first path it will keep searching while it can move
     std::cout << "\nEND\n";
 
     std::cout << "After Search\n";
