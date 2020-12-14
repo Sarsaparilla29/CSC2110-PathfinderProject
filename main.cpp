4d72de3bf01a006cd27f00dbdb35311cdd00ce53
Sarsaparilla29 <jacobthebennett@gmail.com>
2020-12-13 22:47:21 -0500

    blob map

--- main.cpp
+++ main.cpp
@@ -26,28 +26,31 @@ int main(){
     //fullmap[0][3].setCellType(0);
     //fullmap[1][0].setCellType(0);
     // row 1
-    fullmap[1][1].setCellType(0);
+    /*fullmap[1][1].setCellType(0);
     fullmap[1][2].setCellType(0);
     //fullmap[1][3].setCellType(0);
     fullmap[1][4].setCellType(0);
-    fullmap[1][5].setCellType(0);
+    fullmap[1][5].setCellType(0);*/
     // row 2
-    fullmap[2][0].setCellType(0);
+    //fullmap[2][0].setCellType(0);
     //fullmap[2][1].setCellType(0);
     fullmap[2][2].setCellType(0);
     fullmap[2][3].setCellType(0);
     fullmap[2][4].setCellType(0);
     //fullmap[2][5].setCellType(0);
     // row 3
-    fullmap[3][0].setCellType(0);
+    //fullmap[3][0].setCellType(0);
     fullmap[3][1].setCellType(0);
     fullmap[3][2].setCellType(0);
-    //fullmap[3][3].setCellType(0);
+    fullmap[3][3].setCellType(0);
     fullmap[3][4].setCellType(0);
     //fullmap[3][5].setCellType(0);
     // row 4
+    fullmap[4][1].setCellType(0);
+    fullmap[4][2].setCellType(0);
+    fullmap[4][3].setCellType(2);
     fullmap[4][4].setCellType(0);
-    fullmap[4][5].setCellType(2);
+    //fullmap[4][5].setCellType(2);
     
     /*fullmap[3][4].setCellType(0);
     fullmap[3][3].setCellType(0);
@@ -67,7 +70,7 @@ int main(){
         std::cout << "\n";
     }
     
-    pathfinder pf(&fullmap[0][0],1,5);
+    pathfinder pf(&fullmap[0][0],2,2);
     pf.search();
     // after searching for first path it will keep searching while it can move
     std::cout << "\nEND\n";
