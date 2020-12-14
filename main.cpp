6bf9d8ebb1de545134b17f52a4de7e4a9fafd282
Jacob Bennett <57199778+Sarsaparilla29@users.noreply.github.com>
2020-12-13 21:36:18 -0500

    final changes

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 #include "cell.h"
-//#include "position.h"
 #include "pathfinder.h"
 
 int main(){
-    // from zoom meeting dec 3
     // create map
     cell fullmap[5][6];
     // fill the map with walls (1)
-    for(int i = 0; i < 6; i++)
-        for(int j = 0; j < 7; j++)
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 6; j++) {
             fullmap[i][j].setCellType(1);
+        }
+    }
 
     std::cout << "After setting all cells to wall(1)\n";
-    for(int i = 0; i < 6; i++){
+    for(int i = 0; i < 5; i++){
         std::cout << " ";
-        for(int j = 0; j < 7; j++){
+        for(int j = 0; j < 6; j++){
             std::cout << fullmap[i][j].getCellType() << " ";
         }
         std::cout << "\n";
@@ -34,35 +34,32 @@ int main(){
     fullmap[3][3].setCellType(0);
     fullmap[3][2].setCellType(0);
     fullmap[4][2].setCellType(0);
-    fullmap[5][2].setCellType(2);
-    //fullmap[0][6].setCellType(1); // set manually to a wall(1)
+    fullmap[4][1].setCellType(2);
+    // need to rebuild solution in VS every time a change is made to the map
 
-    //fullmap[0][6].setCellType(1); // for some reason this cell changes to 0 when [1][0] is set to 0
-    // initial position will be 1,0
+    // initial position will be (1,0)
     
     std::cout << "Created Path\n";
-    for(int i = 0; i < 6; i++){
+    for(int i = 0; i < 5; i++){
         std::cout << " ";
-        for(int j = 0; j < 7; j++){
+        for(int j = 0; j < 6; j++){
             std::cout << fullmap[i][j].getCellType() << " ";
         }
         std::cout << "\n";
     }
     
-    pathfinder pf(&fullmap[0][0],0,1);
+    pathfinder pf(&fullmap[0][0],1,0);
     pf.search();
-    std::cout << "END\n";
+    std::cout << "\nEND\n";
 
+    std::cout << "After Search\n";
+    for (int i = 0; i < 5; i++) {
+        std::cout << " ";
+        for (int j = 0; j < 6; j++) {
+            std::cout << fullmap[i][j].getCellType() << " ";
+        }
+        std::cout << "\n";
+    }
 
-
-    //cellObj.setCellType(arr[0][0]);
-
-    // pointer to array
-    //int *Ptr = &arr[0][0];
-
-    // search function
-    //pathObj.search();
-
-    // end of program
-    //return 0;
+    return 0;
 }
